3d97c5979c57285352912717c84640b54201019f
SlayerWx <slayerwx95@gmail.com>
2019-10-23 13:05:07 -0300

    Hecho por Simon:Colisiones casi terminadas

---
+++ Assets/Balls/script/collision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    void Update()
+    {
+        
+    }
+    public void myCollision()
+    {
+        Debug.Log("COLISION!!");
+    }
+}
---
+++ Assets/Balls/script/managerCheckCollision.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerCheckCollision : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] balls;
+    [SerializeField]
+    private collision[] ballsCollision;
+    void Start()
+    {
+        for(int i = 0; i < balls.Length;i++)
+        {
+            ballsCollision[i] = balls[i].GetComponent<collision>();
+        }
+    }
+
+    void Update()
+    {
+        CheckCollisions();
+    }
+    private void CheckCollisions()
+    {
+        Vector2 positionBall1;
+        Vector2 positionBall2;
+        float distance = 0;
+        float comparation;
+        for (ushort i = 0; i < balls.Length;i++)
+        {
+            positionBall1 = balls[i].position;
+            for(ushort t = 0; t < balls.Length;t++)
+            {
+                if (t != i)
+                {
+                    positionBall2 = balls[t].position;
+                    distance = Mathf.Pow(Mathf.Abs(positionBall1.x - positionBall2.x), 2) + 
+                               Mathf.Pow(Mathf.Abs(positionBall1.y - positionBall2.y), 2);
+
+                    comparation = distance - (balls[i].localScale.x + 0.5f);
+                    if(comparation <= 0)
+                    {
+                        ballsCollision[i].myCollision();
+                        ballsCollision[t].myCollision();
+                    }
+                }
+            }
+        }
+    }
+}
