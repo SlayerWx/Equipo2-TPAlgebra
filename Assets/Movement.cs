b7241191ee926fa6a0ace82a39b4f6f26b6bb5e5
hernan gossa <hernanagustin.g64@hotmail.com>
2019-10-25 12:10:50 -0300

    Logica de aceleracion (Provisional)

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private const int mass = 160; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private Vector2 BallMovement(Vector2 force,float meters)
+    {
+        Vector2 resultAcceleration;
+        resultAcceleration.x = 0;
+        resultAcceleration.y = 0;
+
+        resultAcceleration.x = force.x / mass;
+        resultAcceleration.y = force.y / mass;
+
+        resultAcceleration.x = resultAcceleration.x / Mathf.Pow(Time.deltaTime, 2);
+        resultAcceleration.y = resultAcceleration.y / Mathf.Pow(Time.deltaTime, 2);
+        Debug.Log("aceleracion conseguida: x:" + resultAcceleration.x + " y:" + resultAcceleration.y);
+        return resultAcceleration;
+    }
+}
