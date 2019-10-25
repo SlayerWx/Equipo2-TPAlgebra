455bb1a84a63aec7d04fe06892ea8369311d3f66
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-10-25 12:58:34 -0300

    ordenando carpetas y moviendo funcion de Movement a CollisionManager

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
-    const int cantBalls = 2;
-    [SerializeField]
-    private Transform[] balls;
+    public GameObject[] balls;
     void Start()
     {
     }
@@ -22,17 +20,18 @@ public class CollisionManager : MonoBehaviour
         float comparation;
         for (ushort i = 0; i < balls.Length; i++)
         {
-            positionBall1 = balls[i].position;
+            positionBall1 = balls[i].transform.position;
             for (ushort t = 0; t < balls.Length; t++)
             {
                 if (t != i)
                 {
-                    positionBall2 = balls[t].position;
+                    positionBall2 = balls[t].transform.position;
                     distance = Mathf.Pow(Mathf.Abs(positionBall1.x - positionBall2.x), 2) +
                                Mathf.Pow(Mathf.Abs(positionBall1.y - positionBall2.y), 2);
 
-                    comparation = distance - (Mathf.Max(balls[i].localScale.x, balls[i].localScale.y) +
-                    (Mathf.Max(balls[t].localScale.x, balls[t].localScale.y)));
+                    comparation = distance - 
+                    (Mathf.Max(balls[i].transform.localScale.x, balls[i].transform.localScale.y) +
+                    (Mathf.Max(balls[t].transform.localScale.x, balls[t].transform.localScale.y)));
                     if (comparation <= 0)
                     {
                         balls[i].GetComponent<BallCollision>().MyCollision();
@@ -41,5 +40,18 @@ public class CollisionManager : MonoBehaviour
                 }
             }
         }
+    }
+    private Vector2 getResultAcceleration(Vector2 force,float mass)
+    {
+        Vector2 resultAcceleration;
+        resultAcceleration.x = 0;
+        resultAcceleration.y = 0;
+        resultAcceleration.x = force.x / mass;
+        resultAcceleration.y = force.y / mass;
+
+        resultAcceleration.x = resultAcceleration.x / Mathf.Pow(Time.deltaTime, 2);
+        resultAcceleration.y = resultAcceleration.y / Mathf.Pow(Time.deltaTime, 2);
+        Debug.Log("aceleracion conseguida: x:" + resultAcceleration.x + " y:" + resultAcceleration.y);
+        return resultAcceleration;
     }
 }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private const int mass = 160;
+    private Vector2 acceleration;
+
+    void Start()
+    {
+        acceleration = new Vector2(0.0f, 0.0f);
+    }
+    void Update()
+    {
+        
+    }
+}
