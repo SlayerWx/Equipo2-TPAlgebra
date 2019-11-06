a834c6e8c1e0f8001b145af91124f91331190f46
ignacio <ignacio.f.l@hotmail.com>
2019-11-06 13:15:26 -0300

    agregado palo, sprite del palo y inputs para rotar sobre la pelota

---
+++ Assets/Scripts/PoleMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))transform.Rotate(0,0,1);
+        if (Input.GetKey(KeyCode.D))transform.Rotate(0, 0, -1); ;
+    }
+}
