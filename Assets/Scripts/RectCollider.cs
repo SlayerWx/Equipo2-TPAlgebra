ca38647c32055a231567081a6dc3044b385267d9
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-11-13 13:03:01 -0300

    acomodando escena para la collision

---
+++ Assets/Scripts/RectCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectCollider : MonoBehaviour
+{
+    public Rect collider;
+    public GameObject anotherGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collider.Contains(anotherGO.transform.position))
+        {
+            Debug.Log("zdfbkzgzsi");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(collider.min, new Vector2(collider.max.x, collider.min.y));
+        Gizmos.DrawLine(collider.min, new Vector2(collider.min.x, collider.max.y));
+        Gizmos.DrawLine(new Vector2(collider.max.x, collider.min.y), collider.max);
+        Gizmos.DrawLine(new Vector2(collider.min.x, collider.max.y), collider.max);
+    }
+}
